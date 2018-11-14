b3866a70be85a79de6bb2262337f4ca037419b6a
parkoon <par_koon@naver.com>
2018-11-14 19:35:24 +0900

    User login API 작성
    ## 알아야할 것
     jwt 정확한 사용법

--- routes/Users.js
+++ routes/Users.js
@@ -24,23 +24,52 @@ users.post('/register', (req, res) => {
     User.findOne({
         email,
     })
-        .then(user => {
-            if (user) {
-                res.json({ error: 'User already exists' })
-            } else {
-                // 비밀번호 암호화 필요
-                User.create(userData)
-                    .then(user => {
-                        res.json({ status: `${user.email} is registered`})
-                    })
-                    .catch(err => {
-                        res.send(`error: ${err}`)
-                    })
+    .then(user => {
+        if (user) {
+            res.json({ error: 'User already exists' })
+        } else {
+            // 비밀번호 암호화 필요
+            User.create(userData)
+                .then(user => {
+                    res.json({ status: `${user.email} is registered` })
+                })
+                .catch(err => {
+                    res.send(`error: ${err}`)
+                })
+        }
+    })
+    .catch(err => {
+        res.send(`error: ${err}`)
+    })
+})
+
+users.post('/login', (req, res) => {
+    const { email } = req.body
+
+    User.findOne({
+        email,
+    })
+    .then(user => {
+        if (user) {
+            const { _id, first_name, last_name, email } = user
+            const payload = {
+                _id,
+                first_name,
+                last_name,
+                email,
             }
-        })
-        .catch(err => {
-            res.send(`error: ${err}`)
-        })
+
+            const token = jwt.sign(payload, process.env.SCRET_KEY, {
+                expiresIn: 1440
+            })
+
+            res.json({ token })
+        } else {
+            res.json({ error: 'User does not exist'})
+        }
+    })
+    .catch(err => { res.send(`error: ${err}`)})
+
 })
 
 module.exports = users
