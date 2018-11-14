0b617a66fdd32190dbc442f5bdfe4387b4a652da
parkoon <par_koon@naver.com>
2018-11-14 19:01:58 +0900

    User model 생성

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+const UserSchema = new Schema({
+    first_name: {
+        type: String
+    },
+    last_name: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true,
+
+    },
+    passowrd: {
+        type: String,
+        required: true,
+
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const User = mongoose.model('users', UserSchema)
+
+module.exports = User
